4b00837f06ed7a6eace8d3a3c7279de8ecee27bd
SDSB <sdsbjunes@gmail.com>
2019-03-06 23:33:09 +0100

    Uses fragments instead of activities

--- Dev/app/src/main/java/com/example/gruppe30in2000/MainActivity.kt
+++ Dev/app/src/main/java/com/example/gruppe30in2000/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gruppe30in2000
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,13 +12,18 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                replaceFragment(HomeFragment())
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
+            R.id.navigation_map -> {
                 message.setText(R.string.title_map)
-                val intent = Intent(this, MapsActivity::class.java)
-                startActivity(intent)
+                replaceFragment(MapFragment())
+
+                //** Koden under brukes til å bytte activity istedenfor fragment
+
+                //val intent = Intent(this, MapsActivity::class.java)
+                //startActivity(intent)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
@@ -33,6 +39,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 
 }
---
+++ Dev/app/src/main/java/com/example/gruppe30in2000/MapFragment.kt
@@ -0,0 +1,41 @@
+package com.example.gruppe30in2000
+
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class MapFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+
+    }
+
+
+
+
+}
