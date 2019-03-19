2ea0afb11e3c7b8bd742d606cb425bb1c93474f6
Steven <steven.hgng@gmail.com>
2019-03-19 14:25:00 +0100

    Implemented swipe card to delete

--- Dev/app/src/main/java/com/example/gruppe30in2000/MainActivity.kt
+++ Dev/app/src/main/java/com/example/gruppe30in2000/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.gruppe30in2000
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
