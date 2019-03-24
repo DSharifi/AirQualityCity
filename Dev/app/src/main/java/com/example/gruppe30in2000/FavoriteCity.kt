b88a11fb5da86118847d0019fefd56782994fafb
Steven <steven.hgng@gmail.com>
2019-03-24 13:55:12 +0100

    starting to implement google place suggestion functionality

--- Dev/app/src/main/java/com/example/gruppe30in2000/FavoriteCity.kt
+++ Dev/app/src/main/java/com/example/gruppe30in2000/FavoriteCity.kt
@@ -1,46 +1,70 @@
 package com.example.gruppe30in2000
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
 import android.location.Address
 import android.location.Geocoder
-import android.nfc.Tag
 import com.github.salomonbrys.kotson.*
 import com.google.gson.Gson
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat.getSystemService
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import java.util.ArrayList
 import android.support.v7.widget.helper.ItemTouchHelper
 import android.util.Log
-import android.view.KeyEvent
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
+import android.widget.*
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.dynamic.SupportFragmentWrapper
+import com.google.android.gms.location.places.GeoDataClient
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import kotlinx.android.synthetic.main.alert_dialog.view.*
+import kotlinx.android.synthetic.main.city_element.view.*
+
 import java.io.IOException
+import java.util.*
 
 
-class FavoriteCity : Fragment() {
-    data class Station(
-        // identifier, expression of interest
-        val eoi: String
-    )
+class FavoriteCity : Fragment(), GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var fView: View
+    private lateinit var placesClient : PlacesClient
+
     private val sharedPREF = "sharedPrefs"
     private val dataSET= "dataset"
 
@@ -77,14 +101,9 @@ class FavoriteCity : Fragment() {
 //        loadData()
         initRecycleView(dataset)
 
-//        println(getAirStations()[3].data.time[0].variables.AQI.value)
-
-
-
-        //Toast.makeText(this, "CURRENT DATA SIZE: ${dataset.size}", Toast.LENGTH_LONG).show()
-
-
         floatingButton.setOnClickListener {
+            onSearchInputEnter(floatingButton.context)
+
             val dialogBuilder = AlertDialog.Builder(this.context!!) // make a dialog builder
             val dialogView = layoutInflater.inflate(R.layout.alert_dialog, null) // get the dialog xml view
             dialogBuilder.setView(dialogView) // set the view into the builder
@@ -96,7 +115,7 @@ class FavoriteCity : Fragment() {
             val addButton = dialogView.findViewById<Button>(R.id.add_button)
             val edit_title = dialogView.findViewById<EditText>(R.id.edit_title)
             val edit_description = dialogView.findViewById<EditText>(R.id.edit_description)
-            val search_text = dialogView.findViewById<EditText>(R.id.search_input)
+            val search_text = dialogView.findViewById<AutoCompleteTextView>(R.id.search_input)
 
             // make a common textWatcher to use for several editText listener
             val textWatcher = object: TextWatcher {
@@ -124,6 +143,7 @@ class FavoriteCity : Fragment() {
 
                         Log.e("Searching", "Serafdashi")
                         geolocate(search_text.text)
+                        hideSoftKeyboard()
 
 
                     }
@@ -183,7 +203,7 @@ false // return false if no change/edits were made
         }
 
 
-
+        // make a swipe controller object to enable swipe card.
         var swipeController = object : SwipeController() {
             override fun deleteItem(pos : Int) {
                 deleteItemAt(pos)
@@ -194,8 +214,130 @@ false // return false if no change/edits were made
 
 
 
+        // Initialize places context and api
+        Places.initialize(this.requireContext(), "AIzaSyCrfEIKJc8Nqz6dPV-Ju1jgCAb-BRek70g")
+
+        // Create a new Places client instance.
+        placesClient = Places.createClient(requireContext())
+
+
+        // Initialize the AutocompleteSupportFragment.
+        // TODO find out if theres another way to hide the search bar in this activity (fragment with id autocomplete_fragment)
+        val autocompleteFragment =  fragmentManager?.findFragmentById(R.id.autocomplete_fragment) as? AutocompleteSupportFragment
+
+        autocompleteFragment?.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS))
+
+        autocompleteFragment?.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+
+            override fun onPlaceSelected(place : Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.name + ", " + place.id)
+            }
+
+            override fun onError(status : Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
+
 
     }
+
+    // Test method for places autocomplete
+    fun onSearchInputEnter(context : Context) {
+
+        val fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS)
+
+        // Start the autocomplete intent.
+        val intent = Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.OVERLAY, fields)
+                .build(context)
+
+        startActivityForResult(intent, 1)
+
+    }
+     /**
+     * Override the activity's onActivityResult(), check the request code, and
+     * do something with the returned place data (in this example it's place name and place ID).
+     */
+    override fun onActivityResult(requestCode : Int , resultCode: Int, data : Intent) {
+        if (requestCode == 1) {
+            when (resultCode) {
+            RESULT_OK -> {
+                val place = Autocomplete.getPlaceFromIntent(data)
+                Log.i(TAG, "Place: " + place.name + ", " + place.id + ", Address: " + place.address)
+//                recyclerView.edit_title.text = place.name as Editable
+
+                // TODO Set the place name/address in title of the cardview
+            } AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    val status = Autocomplete.getStatusFromIntent(data)
+                    Log.i(TAG, status.statusMessage)
+            } RESULT_CANCELED -> {
+                    // The user canceled the operation.
+            }
+            }
+        }
+
+    }
+
+
+    // Fetch a spesifct place by id
+    fun fetchPlaces() {
+
+        // Define a Place ID.
+        var placeId = "INSERT_PLACE_ID_HERE"
+
+        // Specify the fields to return.
+        val placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS)
+
+        // Construct a request object, passing the place ID and fields array.
+        val request = FetchPlaceRequest.builder(placeId, placeFields).build()
+
+        // Add a listener to handle the response.
+
+        placesClient.fetchPlace(request).addOnSuccessListener {
+            response ->
+              val place = response.place
+              Log.i(TAG, "Place found: " + place.name)
+            }.addOnFailureListener {
+                exception ->
+                Log.e(TAG, "Place not found: " + exception.message)
+
+            }
+        }
+
+    fun placeAutocomplete() {
+        val token = AutocompleteSessionToken.newInstance()
+
+        // Create a RectangularBounds object.
+        val bounds = RectangularBounds.newInstance(
+          LatLng(-33.880490, 151.184363),
+          LatLng(-33.858754, 151.229596));
+        // Use the builder to create a FindAutocompletePredictionsRequest.
+        val request = FindAutocompletePredictionsRequest.builder()
+        // Call either setLocationBias() OR setLocationRestriction().
+           .setLocationBias(bounds)
+           //.setLocationRestriction(bounds)
+           .setCountry("au")
+           .setTypeFilter(TypeFilter.ADDRESS)
+           .setSessionToken(token)
+           .setQuery("query")
+           .build()
+
+        placesClient.findAutocompletePredictions(request).addOnSuccessListener {
+                response ->
+            for (prediction in response.autocompletePredictions) {
+                Log.i(TAG, prediction.placeId)
+                Log.i(TAG, prediction.getPrimaryText(null).toString())
+            }
+        }.addOnFailureListener {
+                exception ->
+                   val apiException = ApiException(Status(1))
+                   Log.e(TAG, "Place not found: " + apiException.statusCode)
+        }
+    }
+
 
     fun moveItem(oldPos: Int, newPos: Int) {
         val fooditem = dataset.get(oldPos)
@@ -213,35 +355,31 @@ false // return false if no change/edits were made
 
     }
 
-//
-//    // Henter livedata for alle målestasjoner
-//    fun getAirStations() : ArrayList<AirQualityStation> {
-//        val stationList = ArrayList<AirQualityStation>()
-//        val stations = getStations()
-//
-//        val gson = Gson()
-//
-//        for (station in stations) {
-//            // Hent stasjonmaaling
-//            val airQualityResponse = khttp.get("https://in2000-apiproxy.ifi.uio.no/weatherapi/airqualityforecast/0.1/?station=${station.eoi}")
-//            stationList.add(gson.fromJson(airQualityResponse.text))
+    fun hideSoftKeyboard() {
+        activity!!.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+//        if (activity?.currentFocus != null) {
+//            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//            inputMethodManager.hideSoftInputFromWindow(edit.windowToken, 0)
 //        }
-//
-//        return stationList
-//    }
-//
-//    fun getAqi(station:Int, time:Int) : Double{
-//        return getAirStations()[station].data.time[time].variables.AQI.value
-//    }
-//
-//    // Returnerer en liste med alle stasjoner
-//    fun getStations() : ArrayList<Station> {
-//        val response = khttp.get("https://in2000-apiproxy.ifi.uio.no/weatherapi/airqualityforecast/0.1/stations")
-//        return Gson().fromJson(response.text)
-//    }
+    }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO save and load data done by user
     /*
     private fun saveData() {
         val sharedPrefs = getSharedPreferences(sharedPREF, Context.MODE_PRIVATE)
