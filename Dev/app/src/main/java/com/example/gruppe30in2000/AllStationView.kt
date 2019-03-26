3b6462fd93ee1e4fad9f0776f4168c96eb1a7c38
Steven <steven.hgng@gmail.com>
2019-03-26 13:19:00 +0100

    new Activity AllStation

---
+++ Dev/app/src/main/java/com/example/gruppe30in2000/AllStationView.kt
@@ -0,0 +1,22 @@
+package com.example.gruppe30in2000
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.google.android.libraries.places.api.net.PlacesClient
+
+class AllStationView : AppCompatActivity() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var fView: View
+    private lateinit var placesClient : PlacesClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_station_view)
+    }
+}
