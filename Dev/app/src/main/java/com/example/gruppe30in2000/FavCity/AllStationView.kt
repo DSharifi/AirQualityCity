77a77e97a984b9171d373d2d5122c70e7c5cc27f
Steven <steven.hgng@gmail.com>
2019-03-26 14:13:39 +0100

    added fds

--- Dev/app/src/main/java/com/example/gruppe30in2000/FavCity/AllStationView.kt
+++ Dev/app/src/main/java/com/example/gruppe30in2000/FavCity/AllStationView.kt
@@ -1,9 +1,10 @@
-package com.example.gruppe30in2000
+package com.example.gruppe30in2000.FavCity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.example.gruppe30in2000.R
 import com.google.android.libraries.places.api.net.PlacesClient
 
 class AllStationView : AppCompatActivity() {
